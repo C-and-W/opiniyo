c4cac0f926b7389367a1ddc42eb86bbda0290273
jacqhernandez <jm.hernandez96@yahoo.com>
2016-04-23 13:26:59 -0200

    fixed name and age

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@ class Report < ActiveRecord::Base
   has_many :tags
   has_many :comments
 
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, :bucket => 'opiniyoph'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
 end
